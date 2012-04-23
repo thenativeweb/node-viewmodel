32f4ae92c941812fd9ae04b71075abf5c776a2e5
Adriano Raiano <araiano@ksw.kaba.com>
2012-04-23 13:34:10 +0200

    first commit

---
+++ test/repositoryTest.js
@@ -0,0 +1,59 @@
+var expect = require('expect.js')
+  , repository = require('../lib/repository');
+
+describe('Repository', function() {
+
+	describe('calling init', function() {
+
+		describe('on read', function() {
+
+			var read = repository.read;
+
+			describe('without options', function() {
+
+				it('it should callback without an error', function(done) {
+
+					read.init(function(err) {
+						expect(err).not.to.be.ok();
+						done();
+					});
+
+				});
+
+			});
+
+			describe('with options containing a type property with the value of', function() {
+
+				describe('an existing db implementation', function() {
+
+					it('it should callback without an error', function(done) {
+
+						read.init({ type: 'inMemory' }, function(err) {
+							expect(err).not.to.be.ok();
+							done();
+						});
+
+					});
+
+				});
+
+				describe('a non existing db implementation', function() {
+
+					it('it should callback with an error', function(done) {
+
+						read.init({ type: 'strangeDb' }, function(err) {
+							expect(err).to.be.ok();
+							done();
+						});
+
+					});
+
+				});
+
+			});
+
+		});
+
+	});
+
+});
